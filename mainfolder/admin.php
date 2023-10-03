0641112e7e6e49356034a3aa9a5d3e2326b6caee
Lajuna <maharzanlazuna3@gmail.com>
2023-10-03 19:04:40 +0545

    i added chart1 and chart2 in the mainfolder folder

--- mainfolder/admin.php
+++ mainfolder/admin.php
@@ -41,8 +41,9 @@ header("Location:cpindex.php");
             <div class="cards">
                 <div class="card">
                     <div class="card-content">
-                        <div class="number">1217</div>
-                        <div class="card-name">Admin</div>
+                        <div class="number">15
+                        </div>
+                        <div class="card-name">Admin Team</div>
                     </div>
                     <div class="icon-box">
                         <div class="icon-box">
@@ -53,31 +54,33 @@ header("Location:cpindex.php");
 
                 <div class="card">
                     <div class="card-content">
-                        <div class="number">1217</div>
-                        <div class="card-name">Admin</div>
+                        <div class="number">19</div>
+                        <div class="card-name">Technical Team</div>
                     </div>
                     <div class="icon-box">
                         <div class="icon-box">
-                            <i class="fas fa-user-graduate"></i>
+                        <i class="fa-solid fa-screwdriver-wrench"></i>
                         </div>
                     </div>
                 </div>
+            </div>
 
-                <div class="card">
-                    <div class="card-content">
-                        <div class="number">1217</div>
-                        <div class="card-name">Admin</div>
-                    </div>
-                    <div class="icon-box">
-                        <div class="icon-box">
-                            <i class="fas fa-user-graduate"></i>
-                        </div>
+                <div class="charts">
+                    <div class="chart">
+                        <h2>Earning (part 12 months) </h2>
+                        <canvas id="lineChart"></canvas>
+</div>
+                    <div class="chart" id="doughnut-chart">
+                        <h2>Employee</h2>
+                        <canvas id="doughnut"></canvas>
                     </div>
                 </div>
-
+            </div>
 
             </div>
-        </div>
+            <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"></script>
+            <script src="chart1.js"></script>
+            <script src="chart2.js"></script>
         <!--<div class="content">
             <p> Showing data of male, female, and others </p>
             <canvas id = "pieChart"> </canvas>
