71f088a5e3068e3682885e7fc052e547fbf4b2cf
pankaj <karkipankaj10@gmail.com>
2023-10-08 22:13:32 +0545

    p

--- mainfolder/Components/Navbar.php
+++ mainfolder/Components/Navbar.php
@@ -1,16 +1,17 @@
 <nav class="navbar navbar-expand-sm ">
         <img src="../img/logo.jpg" class="logo">
-        <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
-        </button>-->
+        </button>
         <!--<div class="collapse navbar-collapse" id="navbarNavDropdown">-->
-          <ul class="navbar-nav">
-            <li class="nav-item active"><a class="nav-link" href="#">Home </a></li>
-            <li class="nav-item"><a class="nav-link" href="#">About Us</a> </li>
-            <li class="nav-item"><a class="nav-link" href="#">Contact Us</a></li>
-            <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
-            <div class="search">
+         <ul class="navbar-nav">
+         <div class="search">
               <input type="text" name="search" id="search" placeholder="Search anything"></div>
+            <li class="nav-item"><a class="nav-link1" href="#">Home </a></li>
+            <li class="nav-item"><a class="nav-link2" href="#">About Us</a> </li>
+            <li class="nav-item"><a class="nav-link3" href="#">Contact Us</a></li>
+            <li class="nav-item"><a class="nav-link4" href="login.php">Login</a></li>
+            
           </ul>
-        </div>
-    </nav>
+      </nav>
+    
