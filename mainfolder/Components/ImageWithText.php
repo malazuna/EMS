ad9f23493aaa7aeb811406aafc9f19a0c49dfe1d
Lajuna <maharzanlazuna3@gmail.com>
2023-10-10 17:29:31 +0545

    slidr file

--- mainfolder/Components/ImageWithText.php
+++ mainfolder/Components/ImageWithText.php
@@ -1,19 +1,19 @@
 <div class = "container" style="width:100%">
     <div class="row">
         <div class="card" style="width: 33%; margin-left:4px;">
-        <img src="https://wallpaper.dog/large/10839283.jpg" class="card-img-top" alt="...">
+        <img src="./img/light.jpeg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <p class="card-text">This image is showing the stage lights and enhanching the stage.</p>
+        </div>
+        </div>
+        <div class="card" style="width: 33%; margin-left:4px;">
+        <img src="./img/sound1.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
         </div>
         <div class="card" style="width: 33%; margin-left:4px;">
-        <img src="https://wallpaper.dog/large/10839283.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        </div>
-        <div class="card" style="width: 33%; margin-left:4px;">
-        <img src="https://wallpaper.dog/large/10839283.jpg" class="card-img-top" alt="...">
+        <img src="./img/led1.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
--- mainfolder/Content/TeamXL.php
+++ mainfolder/Content/TeamXL.php
@@ -2,14 +2,13 @@
 <div class="container-fluid">
   <div class="row">
     <div class="col-xl-6 col-lg-6 col-md-6" style="border:1px solid #ddd">
-      <img src="./img/ban1.jpg" alt="responsive webite" class="img-fluid">
+      <img src="./img/light1.jpg" alt="responsive webite" class="img-fluid">
     </div>
     <div class="col-xl-6 col-lg-6 col-md-6" style="border:1px solid #ddd">
       <h2>Introduction</h2>
-      <p>TeamXL is audio-visual system provider. We make you event more beautiful and decorative.
-      TeamXL is audio-visual system provider. We make you event more beautiful and decorative.
-      TeamXL is audio-visual system provider. We make you event more beautiful and decorative.
-      TeamXL is audio-visual system provider. We make you event more beautiful and decorative.
+      <p>TeamXL is a profesional AVL system provider. TeamXL provides equipment and services related to audio, video, and lighting for various events and occasions. It plays a crucial role in ensuring the technical aspects of events, such as concerts, corporate presentations, weddings, conferences, and other gatherings, run smoothly and effectively.
+</p><p>
+      It offers a wide range of audio-visual equipment for rent. This equipment can include microphones, speakers, projectors, screens, cameras, truss, lighting fixtures, mixers, amplifiers, and more. Customers can rent this equipment for a specified period to meet their event's needs. In addition, it provides technical support and expertise. They offer services like setup, installation, operation, and troubleshooting during events to ensure that everything functions correctly.
       </p>
     </div>
   </div>
